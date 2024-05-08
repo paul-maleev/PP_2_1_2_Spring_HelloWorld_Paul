58d1476b61e05beb5fb924de4d6460f6e3177901
Pavel Maleev <paul.maleev@gmail.com>
2024-05-08 13:59:40 +0300

    Практическая задача 2.1.3 Java pre-project. Задача 2.1.2. Spring. Первые шаги. HelloWorld. сделано

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.getMessage());
+        Cat bean2Cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getName());
+        System.out.println(beanCat == bean2Cat);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,21 @@
+public class Cat {
+
+
+    private String name;
+
+    // Конструктор
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+}
